d5fa4661dad65b3b6ae6d6e1b6479ca08502959e
varunthanneeru <varuntej196@gmail.com>
2025-11-24 21:18:24 +0530

    commit 2

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
